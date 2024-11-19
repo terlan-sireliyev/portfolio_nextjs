89e51941965f6ff578f9e77ff17ff3970c458a3b
TerlaN2233 <95983928+TerlaN2233@users.noreply.github.com>
2024-11-19 12:58:52 +0300

    change mobile version

--- components/user/mobileHeader/index.jsx
+++ components/user/mobileHeader/index.jsx
@@ -1,17 +1,12 @@
 "use client";
 
-import React, { useState } from "react";
-import MenuIcon from "@mui/icons-material/Menu";
+import React from "react";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import styles from "./style.module.css";
 import Link from "next/link";
 import { NAV_LINKS } from "../right/navbar/navbar_mock";
-// import Mobil_Navbar from "../right/navbar/Mobil_Navbar";
 
 const index = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => setIsOpen(!isOpen);
   return (
     <>
       <div className="bg-leftBgOne text-leftTextColorGray w-full h-10 flex items-center justify-between px-2 lg:hidden relative">
@@ -29,24 +24,12 @@ const index = () => {
         <div className=" w-full">
           <ul className="flex justify-center items-center gap-4">
             {NAV_LINKS.map((item) => (
-              <li>
+              <li key={item.id}>
                 <Link href={item.href}>{item.title}</Link>
               </li>
             ))}
           </ul>
         </div>
-        {/* <div className="text-lineProgressColor cursor-pointer">
-          <MenuIcon
-            onClick={toggleMenu}
-            className={`${styles.animationMobilHamburger}`}
-            style={{
-              fontSize: "30px",
-              border: "1px solid #1FDF64",
-              borderRadius: "10px",
-              padding: "4px",
-            }}
-          />
-        </div> */}
       </div>
     </>
   );
