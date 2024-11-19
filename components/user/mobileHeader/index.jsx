63e57f1b1e3d666b3053421257cfebc0f4e9719c
TerlaN2233 <95983928+TerlaN2233@users.noreply.github.com>
2024-11-19 12:32:35 +0300

    change mobile version

--- components/user/mobileHeader/index.jsx
+++ components/user/mobileHeader/index.jsx
@@ -1,15 +1,21 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import styles from "./style.module.css";
+import Link from "next/link";
+import { NAV_LINKS } from "../right/navbar/navbar_mock";
+// import Mobil_Navbar from "../right/navbar/Mobil_Navbar";
 
 const index = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
   return (
     <>
       <div className="bg-leftBgOne text-leftTextColorGray w-full h-10 flex items-center justify-between px-2 lg:hidden relative">
-        <div className="text-lineProgressColor cursor-pointer">
+        <div className="text-lineProgressColor cursor-pointer relative">
           <MoreVertIcon
             className={`${styles.animationMobilHamburger}`}
             style={{
@@ -20,8 +26,18 @@ const index = () => {
             }}
           />
         </div>
-        <div className="text-lineProgressColor cursor-pointer">
+        <div className=" w-full">
+          <ul className="flex justify-center items-center gap-4">
+            {NAV_LINKS.map((item) => (
+              <li>
+                <Link href={item.href}>{item.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* <div className="text-lineProgressColor cursor-pointer">
           <MenuIcon
+            onClick={toggleMenu}
             className={`${styles.animationMobilHamburger}`}
             style={{
               fontSize: "30px",
@@ -30,7 +46,7 @@ const index = () => {
               padding: "4px",
             }}
           />
-        </div>
+        </div> */}
       </div>
     </>
   );
---
+++ components/user/right/navbar/navbar_mock.jsx
@@ -0,0 +1,17 @@
+export const NAV_LINKS = [
+  {
+    id: 1,
+    title: "Home",
+    href: "/",
+  },
+  {
+    id: 2,
+    title: "Contact",
+    href: "/contact",
+  },
+  {
+    id: 3,
+    title: "Portfolio",
+    href: "/portfolio",
+  },
+];
