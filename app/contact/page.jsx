dc68419d1d767cca58b6563aaacf9682cd79b87b
terlan-sireliyev <sireliyev95@list.r>
2025-01-10 00:01:25 +0400

    send contact forms

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,4 +1,4 @@
-"use client"; 
+"use client";
 import React, { useRef } from "react";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
@@ -13,23 +13,24 @@ const Page = () => {
     e.preventDefault();
 
     emailjs
-    .sendForm('service_810xcvr', 'template_280qaf4', form.current, {
-      publicKey: 'bIeMVkO1hN21lcoG7',
-    })
-    .then(
-      () => {
-        console.log('SUCCESS!');
-      },
-      (error) => {
-        console.error('FAILED...', error.text);
-      }
-    );}
+      .sendForm('service_810xcvr', 'template_280qaf4', form.current, {
+        publicKey: 'bIeMVkO1hN21lcoG7',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.error('FAILED...', error.text);
+        }
+      );
+  }
 
   return (
     <div>
       <div className="bg-leftBgOne w-[full] h-[290px]  max-md:h-[290px]  p-4 rounded-border5">
         <h1 className="text-leftTextColorWhite font-bold py-2 mt-4">
-        Əlaqə məlumatı
+          Əlaqə məlumatı
         </h1>
         <div className="grid grid-cols-1 max-md:grid-cols-1 gap-4 mt-4">
           <div className="bg-mainBgColor border border-leftTextColorGreen  rounded-border15 p-6 flex flex-col gap-4">
@@ -82,7 +83,7 @@ const Page = () => {
             <div className="w-[50%] max-lg:w-full my-2 p-2">
               <input
                 className="w-full py-2 px-[8px]  rounded-border5 bg-mainBgColor text-leftTextColorWhite"
-                type="email" 
+                type="email"
                 name="from_email"
                 placeholder="Email"
               />
@@ -102,15 +103,15 @@ const Page = () => {
           </div>
         </form>
       </div>
-      <div className="p-4 ">
-        <div className="bg-leftBgOne border border-leftTextColorGreen flex justify-between  rounded-border5 p-2">
-          <div className="text-leftTextColorGray max-sm:text-8">
-            C 2024 all right reseerved
+      <div className="p-4 bg-leftBgOne border border-leftTextColorGreen rounded-border5  max-md:w-[95%] max-sm:w-[93.5%] w-[97%] m-auto">
+        <div className="text-leftTextColorGray py-2 pb-[10px] border-b-2 border-leftTextColorGreen text-center max-sm:text-12">
+          C 2024 bütün hüquqlar qorunur
+        </div>
+        <div className=" flex justify-between  rounded-border5 py-2 pt-[10px]">
+          <div className="text-leftTextColorGray max-sm:text-12">
+            By Tharlan
           </div>
-          <div className="text-leftTextColorGray max-sm:text-8">
-            Made with by Tharlan
-          </div>
-          <div className="text-leftTextColorGray max-sm:text-8 flex items-center gap-2">
+          <div className="text-leftTextColorGray max-sm:text-12 flex items-center gap-2">
             <SiGmail />
             sireliyevterlan95@gmail.com
           </div>
