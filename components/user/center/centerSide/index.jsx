46c7ef76e8d5f2fe8a222bafccbb6dd777eaeb18
terlan-sireliyev <sireliyev.95@list.ru>
2025-06-30 16:27:59 +0400

    Yeni dəyişikliklər əlavə edildi

--- components/user/center/centerSide/index.jsx
+++ components/user/center/centerSide/index.jsx
@@ -39,18 +39,33 @@ const Index = () => {
           </div>
           {/* Replaced <img> with <Image> for optimization */}
           <div className="max-lg:hidden  absolute inset-y-[105px] right-0  h-[200px] w-[200px] ">
-            <Image
+            {/* <Image
               src="/img/emoji.png" // Path to your image
               alt="Emoji"
               width={200} // Set width
               height={200} // Set height
               className="w-full"
+            /> */}
+            <Image
+              src="/img/img1.jpg"
+              alt="Təsvir"
+              width={600}
+              height={400}
+              priority // <-- bunu əlavə et
             />
           </div>
         </div>
 
         <div
-          className={`p-2 h-[319px] text-leftTextColorWhite overflow-y-auto [&::-webkit-scrollbar]:hidden   grid grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-4 `}
+          className={`
+                  p-2 
+                  text-leftTextColorWhite 
+                  overflow-y-auto 
+                  [&::-webkit-scrollbar]:hidden 
+                  grid grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-4 
+                  h-[319px] 
+                  lg:h-[calc(100vh-100px)]
+                `}
         >
           <Expresties
             title={"JavaScript and Frameworks"}
@@ -69,6 +84,36 @@ const Index = () => {
             title={"Backend and Database Management"}
             description={
               "Node.js: Writing efficient, scalable server-side logic for web applications. Express.js: Creating RESTful APIs and middleware for streamlined backend functionality. MongoDB: Expertise in NoSQL database management. Using Mongoose for schema-based data modeling. Implementing CRUD operations and optimizing database queries."
+            }
+          />
+          <Expresties
+            title={"MERN"}
+            description={
+              "I am a skilled MERN stack developer with extensive experience in building robust web applications using MongoDB, Express.js, React.js, and Node.js. I possess a strong understanding of server-side rendering, API integration, and database management. With my expertise, I can deliver dynamic and efficient web solutions tailored to meet the unique requirements of clients."
+            }
+          />
+          <Expresties
+            title={"MERN"}
+            description={
+              "I am a skilled MERN stack developer with extensive experience in building robust web applications using MongoDB, Express.js, React.js, and Node.js. I possess a strong understanding of server-side rendering, API integration, and database management. With my expertise, I can deliver dynamic and efficient web solutions tailored to meet the unique requirements of clients."
+            }
+          />
+          <Expresties
+            title={"MERN"}
+            description={
+              "I am a skilled MERN stack developer with extensive experience in building robust web applications using MongoDB, Express.js, React.js, and Node.js. I possess a strong understanding of server-side rendering, API integration, and database management. With my expertise, I can deliver dynamic and efficient web solutions tailored to meet the unique requirements of clients."
+            }
+          />
+          <Expresties
+            title={"MERN"}
+            description={
+              "I am a skilled MERN stack developer with extensive experience in building robust web applications using MongoDB, Express.js, React.js, and Node.js. I possess a strong understanding of server-side rendering, API integration, and database management. With my expertise, I can deliver dynamic and efficient web solutions tailored to meet the unique requirements of clients."
+            }
+          />
+          <Expresties
+            title={"MERN"}
+            description={
+              "I am a skilled MERN stack developer with extensive experience in building robust web applications using MongoDB, Express.js, React.js, and Node.js. I possess a strong understanding of server-side rendering, API integration, and database management. With my expertise, I can deliver dynamic and efficient web solutions tailored to meet the unique requirements of clients."
             }
           />
           <Expresties
